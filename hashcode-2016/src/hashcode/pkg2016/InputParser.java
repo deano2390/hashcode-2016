6ab0f854a1de7b395fd3575dd09b4bc1da5c2c61
Dean Wild <dean.wild@wizix.com>
2016-02-11 19:53:10 +0000

    parsing done

--- hashcode-2016/src/hashcode/pkg2016/InputParser.java
+++ hashcode-2016/src/hashcode/pkg2016/InputParser.java
@@ -5,8 +5,12 @@
  */
 package hashcode.pkg2016;
 
+import hashcode.pkg2016.models.Drone;
 import hashcode.pkg2016.models.Grid;
+import hashcode.pkg2016.models.Order;
+import hashcode.pkg2016.models.OrderItem;
 import hashcode.pkg2016.models.Product;
+import hashcode.pkg2016.models.Warehouse;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -46,18 +50,27 @@ public class InputParser {
             
             grid.rows = Integer.parseInt(strValues[0]);
             grid.columns = Integer.parseInt(strValues[1]);            
-            int droneCount = Integer.parseInt(strValues[3]);
-            int turns = Integer.parseInt(strValues[4]);
-            int maxPayload = Integer.parseInt(strValues[5]);
-                       
+            
+            int droneCount = Integer.parseInt(strValues[2]);
+            
+            for (int i = 0; i < droneCount; i++) {
+                grid.drones.add(new Drone());
+            }
+            
+            
+            int turns = Integer.parseInt(strValues[3]);
+            grid.turns = turns;
+            
+            int maxPayload = Integer.parseInt(strValues[4]);
+            grid.maxPayload = maxPayload;
+            
             strLine = br.readLine();            //skip a line
             strLine = br.readLine();    // product weights        
             strValues = strLine.split(" ");
             
             int productTypeCount = strValues.length;
-            
-            grid.products = new ArrayList<>();
-            
+           
+                       
             for (int i = 0; i < productTypeCount; i++) {
                 Product product = new Product();
                 product.type = i;
@@ -65,12 +78,58 @@ public class InputParser {
                 grid.products.add(product);
             }
             
+            ArrayList<Product> allProducts = grid.products;
+            
+            strLine = br.readLine();    // warehouse count
+            int warehouseCount = asInt(strLine);
+            
+            for (int i = 0; i < warehouseCount; i++) {
+                strLine = br.readLine();    // warehouse location
+                strValues = strLine.split(" ");
+                Warehouse warehouse = new Warehouse();
+                warehouse.X = asInt(strValues[0]);
+                warehouse.Y = asInt(strValues[1]);
+                
+                strLine = br.readLine();    // warehouse contents
+                strValues = strLine.split(" ");
+                
+                for (int productID = 0; productID < productTypeCount; productID++) {                   
+                    warehouse.products.put(allProducts.get(productID), asInt(strValues[productID]));                    
+                }
+                
+                grid.warehouses.add(warehouse);                
+                
+            }
+            
+            strLine = br.readLine();    // order count
+            int orderCount = asInt(strLine);
             
             
-            
-            
-            
-            
+            for (int i = 0; i < orderCount; i++) {
+                strLine = br.readLine();    // order destination
+                strValues = strLine.split(" ");
+                Order order = new Order();
+                order.X = asInt(strValues[0]);
+                order.Y = asInt(strValues[1]);
+                
+                strLine = br.readLine();    // order item count
+                int itemCount = asInt(strLine);
+                
+                strLine = br.readLine();    // order item quantities
+                strValues = strLine.split(" ");
+                
+                for (int itemNumber = 0; itemNumber < itemCount; itemNumber++) {
+                    int productID = asInt(strValues[itemNumber]);
+                    Product product = allProducts.get(productID);
+                    OrderItem orderItem = new OrderItem();
+                    orderItem.product = product;
+                    orderItem.quantity = 1;              
+                    order.items.add(orderItem);
+                }  
+                
+                
+                grid.orders.add(order);
+            }    
 
             br.close();
 
@@ -84,7 +143,7 @@ public class InputParser {
     }
     
     int asInt(String in){
-        return Integer.parseInt(in);
+        return Integer.parseInt(in.trim());
     }
 
 }
