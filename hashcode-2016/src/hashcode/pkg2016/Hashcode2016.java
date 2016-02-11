c9f322d2fe451c2b24d7e3eceffa28bbecb60e70
Dean Wild <dean.wild@wizix.com>
2016-02-11 20:59:54 +0000

    process all three problems

--- hashcode-2016/src/hashcode/pkg2016/Hashcode2016.java
+++ hashcode-2016/src/hashcode/pkg2016/Hashcode2016.java
@@ -7,6 +7,7 @@ package hashcode.pkg2016;
 
 import hashcode.pkg2016.models.Grid;
 import java.io.File;
+import java.util.ArrayList;
 
 /**
  *
@@ -18,18 +19,29 @@ public class Hashcode2016 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        InputParser parser = new InputParser(new File(args[0]));
-        Grid grid = parser.parse();
-        Calculator calculator = new Calculator(grid);
-        String[] output = calculator.calculate();
-        
+
+        ArrayList<String> attempts = new ArrayList<>();
+
+        attempts.add("busy_day");
+        attempts.add("mother_of_all_warehouses");
+        attempts.add("redundancy");
+
+        for (String attempt : attempts) {
+            
+            String inFile = "assets/" + attempt + ".in";
+            String outFile  = attempt + ".out";
+            InputParser parser = new InputParser(new File(inFile));
+            Grid grid = parser.parse();
+            Calculator calculator = new Calculator(grid);
+            String[] output = calculator.calculate();
+
 //        System.out.println(output.length);
 //        for (String out : output) {
 //            System.out.println(out);
 //        }
-        
-        InstructionWriter.writeInstructions(output, args[1]);
+            InstructionWriter.writeInstructions(output, outFile);
+        }
+
     }
-    
+
 }
