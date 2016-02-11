294ec68c6ac880023892e17d9b8df76d95527cb4
Neil C Smith <neilcsmith.net@googlemail.com>
2016-02-11 19:34:50 +0000

    Add max time

--- hashcode-2016/src/hashcode/pkg2016/models/Grid.java
+++ hashcode-2016/src/hashcode/pkg2016/models/Grid.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hashcode.pkg2016.models;
 
 import java.util.ArrayList;
@@ -15,6 +11,7 @@ public class Grid {
     
     public int rows;
     public int columns;
+    public int maximumTime;
     public ArrayList<Drone> drones;
     public ArrayList<Warehouse> warehouses;
     public ArrayList<Order> orders;
