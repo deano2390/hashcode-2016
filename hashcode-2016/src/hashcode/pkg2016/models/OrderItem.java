8bb22d2d14c0d5ffa17c6bbcbcd6c80f564a3545
Dean Wild <dean.wild@wizix.com>
2016-02-11 19:00:19 +0000

    create models

---
+++ hashcode-2016/src/hashcode/pkg2016/models/OrderItem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcode.pkg2016.models;
+
+/**
+ *
+ * @author deanwild
+ */
+public class OrderItem {
+    
+}
